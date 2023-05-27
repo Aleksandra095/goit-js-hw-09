230225a7650928a67c6db48acf8e9f5f89b27c87
Aleksandra095 <92690797+Aleksandra095@users.noreply.github.com>
2023-05-27 09:03:20 +0000

    Deploying to gh-pages from @ Aleksandra095/goit-js-hw-09@73743527e61c6926cc88b610423eb904e8dfe0aa 🚀

---
+++ 01-color-switcher.423a0841.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let e=null;t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(n){e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(n){clearInterval(e),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.423a0841.js.map
