1b67dc92a12a95c4461e7b7d5998f043bc134b74
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-05-27 23:29:05 +0300

    fix2.3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,17 +3,17 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 window.onload = () => {
     const datePicker = flatpickr('#datetime-picker', {
-        enableTime: true,
-        time_24hr: true,
-        defaultDate: new Date(),
-        minuteIncrement: 1,
-        onClose(selectedDates) {
-        const selectedDate = selectedDates[0];
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+const selectedDate = selectedDates[0];
 
-        if (selectedDate.getTime() < Date.now()) {
-            window.alert('Please choose a date in the future');
-        } else {
-            startTimer(selectedDate);
+if (selectedDate.getTime() < Date.now()) {
+window.alert('Please choose a date in the future');
+    } else {
+    startTimer(selectedDate);
         }
     },
 });
@@ -22,57 +22,66 @@ window.onload = () => {
     const minutesElement = document.querySelector('[data-minutes]');
     const secondsElement = document.querySelector('[data-seconds]');
     const startButton = document.querySelector('[data-start]');
-
     let countdownInterval;
+    let isTimerRunning = false;
 
     startButton.addEventListener('click', () => {
     const selectedDate = datePicker.selectedDates[0];
         if (!selectedDate) {
-            window.alert('Please choose a date and time');
+    window.alert('Please choose a date and time');
         return;
-    }
+}
 
-    if (selectedDate.getTime() < Date.now()) {
-        window.alert('Please choose a date in the future');
+        if (selectedDate.getTime() < Date.now()) {
+    window.alert('Please choose a date in the future');
         return;
-    }
+}
 
+        if (!isTimerRunning) {
     startTimer(selectedDate);
+    isTimerRunning = true;
+    }
 });
-    function startTimer(selectedDate) {
-        clearInterval(countdownInterval);
+
+function startTimer(selectedDate) {
+    clearInterval(countdownInterval);
     countdownInterval = setInterval(() => {
-        const timeDifference = selectedDate.getTime() - Date.now();
+    const timeDifference = selectedDate.getTime() - Date.now();
+
         if (timeDifference <= 0) {
-        updateTimerValues(0, 0, 0, 0);
-        clearInterval(countdownInterval);
-            startButton.disabled = false;
+    updateTimerValues(0, 0, 0, 0);
+    clearInterval(countdownInterval);
+    startButton.disabled = false;
+    isTimerRunning = false;
         } else {
-        const { days, hours, minutes, seconds } = convertMs(timeDifference);
-        updateTimerValues(days, hours, minutes, seconds);
-        }
-    }, 1000);
-        startButton.disabled = true;
-        datePicker.close();
+    const { days, hours, minutes, seconds } = convertMs(timeDifference);
+    updateTimerValues(days, hours, minutes, seconds);
     }
-    function updateTimerValues(days, hours, minutes, seconds) {
-        daysElement.textContent = addLeadingZero(days);
-        hoursElement.textContent = addLeadingZero(hours);
-        minutesElement.textContent = addLeadingZero(minutes);
-        secondsElement.textContent = addLeadingZero(seconds);
+}, 1000);
+    startButton.disabled = true;
+    datePicker.close();
+}
+
+function updateTimerValues(days, hours, minutes, seconds) {
+    daysElement.textContent = addLeadingZero(days);
+    hoursElement.textContent = addLeadingZero(hours);
+    minutesElement.textContent = addLeadingZero(minutes);
+    secondsElement.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+    return value.toString().padStart(2, '0');
     }
-    function addLeadingZero(value) {
-        return value.toString().padStart(2, '0');
-    }
-    function convertMs(ms) {
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-        const days = Math.floor(ms / day);
-        const hours = Math.floor((ms % day) / hour);
-        const minutes = Math.floor(((ms % day) % hour) / minute);
-        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
     return { days, hours, minutes, seconds };
     }
 };
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+let formData = {};
+let position = 1;
+const refs = {
+  form: document.querySelector('.form'),
 }
+const createPromise = (position, delay) => {
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    const idTimeout = setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay)
+  })
+}
+const onFormData = (e) => {
+  formData[e.target.name] = Number(e.target.value)
+}
+const onSubmitСreatePromisses = (e) => {
+  e.preventDefault();
+
+  for (let i = 0; i < formData.amount; i += 1) {
+    const idInterval = setInterval(() => {
+      createPromise(position, formData.delay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+        });
+      formData.delay += formData.step
+      position += 1
+      clearInterval(idInterval);
+    }, formData.step);
+  };
+}
+refs.form.addEventListener('input', onFormData)
+refs.form.addEventListener('submit', onSubmitСreatePromisses)
