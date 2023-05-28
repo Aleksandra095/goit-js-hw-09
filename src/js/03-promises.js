8f5a097c6cf473a2e63dece737b4458f619d0b58
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-05-28 21:13:34 +0300

    bugfix3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,40 +4,35 @@ let formData = {};
 let position = 1;
 const refs = {
   form: document.querySelector('.form'),
-}
+};
 const createPromise = (position, delay) => {
-  const shouldResolve = Math.random() > 0.3;
-
+const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
     const idTimeout = setTimeout(() => {
       if (shouldResolve) {
-        resolve({ position, delay })
+        resolve({ position, delay });
       } else {
-        reject({ position, delay })
+        reject({ position, delay });
       }
-    }, delay)
-  })
-}
+    }, delay);
+  });
+};
 const onFormData = (e) => {
-  formData[e.target.name] = Number(e.target.value)
-}
-const onSubmitСreatePromisses = (e) => {
+  formData[e.target.name] = Number(e.target.value);
+};
+const onSubmitCreatePromises = (e) => {
   e.preventDefault();
-
   for (let i = 0; i < formData.amount; i += 1) {
-    const idInterval = setInterval(() => {
-      createPromise(position, formData.delay)
-        .then(({ position, delay }) => {
-          Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-        })
-        .catch(({ position, delay }) => {
-          Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-        });
-      formData.delay += formData.step
-      position += 1
-      clearInterval(idInterval);
-    }, formData.step);
-  };
-}
-refs.form.addEventListener('input', onFormData)
-refs.form.addEventListener('submit', onSubmitСreatePromisses)
+    const delay = formData.delay + i * formData.step;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    position += 1;
+  }
+};
+refs.form.addEventListener('input', onFormData);
+refs.form.addEventListener('submit', onSubmitCreatePromises);
